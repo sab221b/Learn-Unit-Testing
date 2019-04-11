9d7dfbc9ff35430c32ca898fcf271ef2f396c5f2
Rajesh <rajesh@twilightits.com>
2019-04-11 21:15:58 +0530

    add: script.start

--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ const subtraction = require('./subtract');
 const multiplication = require('./multiply');
 const division = require('./divide');
 
+addition.add(58, 42)
+    .then(function (res) {
+        console.log(res)
+    })
+
+subtraction.subtract(19,6)
+.then(function(res) {
+    console.log(res)
+})
+
+multiplication.multiply(23,12)
+.then(function(res) {
+    console.log(res)
+})
+
+division.divide(22,7)
+.then(function(res) {
+    console.log(res)
+})
+.catch(function(err) {
+    console.log('error', err)
+})
 
 module.exports = {
     addition, subtraction, multiplication, division
